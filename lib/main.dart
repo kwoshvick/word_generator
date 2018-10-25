ac78626380acac5bf4e7cab12ee064b00286a11a
kwoshvick <kwoshvick@gmail.com>
2018-10-25 15:06:26 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+//    final wordPair = WordPair.random();
+    return MaterialApp(
+      title: "My App ",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My App'),
+        ),
+        body: Center(
+//          child: Text("Hello "),
+//            child: Text(wordPair.asPascalCase),
+            child: RandomWords(),
+        ),
+      ),
+    );
+  }
+}
+
+
+class RandomWordsState extends State<RandomWords> {
+  // TODO Add build method
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
+
+
+
+
+
+
+
+
+
+//import 'package:flutter/material.dart';
+//
+//void main() => runApp(MyApp());
+//
+//class MyApp extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: 'Welcome to Flutter',
+//      home: Scaffold(
+//        appBar: AppBar(
+//          title: Text('Welcome to Flutter'),
+//        ),
+//        body: Center(
+//          child: Text('Hello World'),
+//        ),
+//      ),
+//    );
+//  }
+//}
